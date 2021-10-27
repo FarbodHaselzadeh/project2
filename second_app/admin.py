a0ca17690dcc32b05ec7050c75619144df2f5f83
root <farbodh@kth.se>
2021-10-27 08:12:44 +0200

    Database

--- second_app/admin.py
+++ second_app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from second_app.models import Topic, AccessRecord, webpage
+admin.site.register(AccessRecord)
+admin.site.register(Topic)
+admin.site.register(webpage)
 
 # Register your models here.
